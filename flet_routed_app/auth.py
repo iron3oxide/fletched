90db158acbe53986bc01cfc44b0a5ad66aa2fd69
jh <jason.hottelet@tuta.io>
2023-01-17 07:42:55 +0100

    fix(decorators): group_required didn't work

--- flet_routed_app/auth.py
+++ flet_routed_app/auth.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, Callable
 
 from flet_routed_app.view_builder import ViewBuilder
 
@@ -14,18 +14,18 @@ def login_required(view_builder_class: Type[ViewBuilder]) -> Type[ViewBuilder]:
     return view_builder_class
 
 
-def group_required(
-    view_builder_class: Type[ViewBuilder], group: str
-) -> Type[ViewBuilder]:
-    def inner(self) -> bool:
-        if (
-            not self.page.auth
-            or not self.page.auth.user
-            or group not in self.page.auth.user.groups
-        ):
-            return False
-        return True
+def group_required(group: str) -> Callable[..., Type[ViewBuilder]]:
+    def wrapper(view_builder_class: Type[ViewBuilder]) -> Type[ViewBuilder]:
+        def inner(self) -> bool:
+            if (
+                not self.page.auth
+                or not self.page.auth.user
+                or group not in self.page.auth.user.groups
+            ):
+                return False
+            return True
 
-    view_builder_class.auth_func = inner
+        view_builder_class.auth_func = inner
+        return view_builder_class
 
-    return view_builder_class
+    return wrapper
--- flet_routed_app/routing.py
+++ flet_routed_app/routing.py
@@ -1,9 +1,9 @@
-from typing import Type
+from typing import Callable, Type
 
 from flet_routed_app.view_builder import ViewBuilder
 
 
-def route(route: str):
+def route(route: str) -> Callable[..., Type[ViewBuilder]]:
     def wrapper(view_builder_class: Type[ViewBuilder]) -> Type[ViewBuilder]:
         view_builder_class.route = route
         return view_builder_class
