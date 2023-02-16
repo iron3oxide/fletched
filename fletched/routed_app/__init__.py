ca91c18fc8715a9b89c64bd964c8220ecbde6814
jh <jason.hottelet@tuta.io>
2023-02-16 19:43:05 +0100

    feat(api): Expose PageNotFoundView

--- fletched/routed_app/__init__.py
+++ fletched/routed_app/__init__.py
@@ -1,5 +1,6 @@
 from fletched.routed_app.app import RoutedApp
 from fletched.routed_app.auth import group_required, login_required
+from fletched.routed_app.page_not_found import PageNotFoundView
 from fletched.routed_app.routing import route
 from fletched.routed_app.state import CustomAppState
 from fletched.routed_app.view_builder import ViewBuilder
