5614e79b6a0af9736f1a65bd2266cdfa355b1928
jh <jason.hottelet@tuta.io>
2023-02-17 04:00:15 +0100

    refactor(routing): simplify template route logic
    it was unnecessary to use flets TemplateRoute when a dict was what was really needed

--- fletched/routed_app/app.py
+++ fletched/routed_app/app.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 from typing import Type
 
@@ -21,7 +22,7 @@ class RoutedApp:
         self.page = page
         self.unauthorized_return_route: str = unauthorized_return_route
         self.last_unauthorized_route: str | None = None
-        self.route_to_viewbuilder: dict[str, ViewBuilder] = {}
+        self.route_pattern_to_viewbuilder: dict[str, ViewBuilder] = {}
 
         if not custom_state:
             self.state = defaultdict(lambda: "not set")
@@ -36,7 +37,8 @@ class RoutedApp:
             )
             view_builder._set_app(self)
             if view_builder.route:
-                self.route_to_viewbuilder[view_builder.route] = view_builder
+                route_pattern = repath.pattern(view_builder.route)
+                self.route_pattern_to_viewbuilder[route_pattern] = view_builder
 
     def _append_view(self, e: ft.RouteChangeEvent) -> None:
         self.page.views.clear()
@@ -52,17 +54,9 @@ class RoutedApp:
         self.page.go(top_view.route)
 
     def _get_view(self, route: str) -> ft.View:
-        template = ft.TemplateRoute(route)
-        for route_template, view_builder in self.route_to_viewbuilder.items():
-            if template.match(route_template):
-                tokens = repath.parse(route_template)
-                parameter_names = [
-                    token["name"] for token in tokens if isinstance(token, dict)
-                ]
-                parameters = {
-                    parameter_name: getattr(template, parameter_name)
-                    for parameter_name in parameter_names
-                    if getattr(template, parameter_name, None)
-                }
-                return view_builder.view_func(parameters)
+        for route_pattern, view_builder in self.route_pattern_to_viewbuilder.items():
+            match = re.match(route_pattern, route)
+            if match:
+                route_params = match.groupdict()
+                return view_builder.view_func(route_params)
         return PageNotFoundView()
