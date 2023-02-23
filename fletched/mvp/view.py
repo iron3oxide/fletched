9bfb44d8ccfceaf48157ff952700577d12c8487b
jh <jason.hottelet@tuta.io>
2023-02-23 06:55:52 +0100

    refactor(view): Adapt MvpView to use MvpRenderer

--- fletched/mvp/view.py
+++ fletched/mvp/view.py
@@ -1,5 +1,3 @@
-from abc import abstractmethod
-from dataclasses import asdict, dataclass
 from typing import List, Optional, Type
 
 import flet as ft
@@ -7,14 +5,12 @@ from abstractcp import Abstract, abstract_class_property
 from pydantic import BaseModel
 
 from fletched.mvp.datasource import MvpDataSource
-from fletched.mvp.error import ErrorMessage
 from fletched.mvp.presenter import MvpPresenter
-from fletched.mvp.protocols import MvpPresenterProtocol
+from fletched.mvp.renderer import MvpRenderer
 from fletched.routed_app import PageNotFoundView, ViewBuilder
 
 
-@dataclass
-class ViewConfig:
+class ViewConfig(BaseModel):
     route: Optional[str] = None
     controls: Optional[List[ft.Control]] = None
     appbar: Optional[ft.AppBar] = None
@@ -28,43 +24,16 @@ class ViewConfig:
     scroll: Optional[ft.ScrollMode] = None
     auto_scroll: Optional[bool] = None
 
+    class Config:
+        arbitrary_types_allowed = True
 
-class MvpView(Abstract, ft.View):
+
+class MvpView(Abstract, MvpRenderer, ft.View):
     ref_map = abstract_class_property(dict[str, ft.Ref])
     config = abstract_class_property(ViewConfig)
 
     def __init__(self) -> None:
-        super().__init__(**asdict(self.config))
-
-    def render(self, model: BaseModel) -> None:
-        page: ft.Page | None = None
-        model_map = model.dict()
-
-        for variable_name, ref in self.ref_map.items():
-
-            model_field_content = model_map[variable_name]
-            control_attribute_name = "value"
-            if not hasattr(ref.current, control_attribute_name):
-                control_attribute_name = "text"
-            if isinstance(model_field_content, ErrorMessage):
-                control_attribute_name = "error_text"
-                model_field_content = model_field_content.message
-
-            control_attribute_content = getattr(ref.current, control_attribute_name)
-
-            if model_field_content == control_attribute_content:
-                continue
-            setattr(ref.current, control_attribute_name, model_field_content)
-
-            if not page:
-                page = ref.current.page
-
-        if page:
-            page.update()
-
-    @abstractmethod
-    def build(self, presenter: MvpPresenterProtocol) -> None:
-        ...
+        super().__init__(**self.config.dict())
 
 
 class MvpViewBuilder(Abstract, ViewBuilder):
